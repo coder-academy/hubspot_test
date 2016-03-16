41ee8f9fc9804a92a5ba2088bd220cad1ba53435
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-03-16 15:47:09 +1100

    removed hubspot intializer

--- config/initializers/hubspot.rb
+++
@@ -1,5 +0,0 @@
-Hubspot.config.hubspot_site = 'demo.app11.hubspot.com'
-Hubspot.config.hubspot_access_token = 'demooooo-oooo-oooo-oooo-oooooooooooo'
-Hubspot.config.hubspot_key = 'demo'
-Hubspot.config.hubspot_portal_id = '62515'
-Hubspot.config.debug_http_output = false
